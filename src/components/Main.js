449d880114ef808387ef6f147a3a032b47ddaab4
Emily <yeouvi29@gmail.com>
2021-10-08 11:12:34 -0700

    modify fetching order

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import Pokedex from "./Pokedex";
 import classes from "./Main.module.css";
 
 const Main = () => {
-  const [getNewNames, setGetNewNames] = useState(true);
+  const [isFetching, setIsFetching] = useState(true);
   const [url, setUrl] = useState(
     "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
   );
@@ -15,43 +15,15 @@ const Main = () => {
 
   console.log("main is rendering");
 
-  const fetchJSONData = useCallback(async (url) => {
-    console.log("fetching Data");
-    const res = await fetch(url);
-    const data = await res.json();
-    return data;
-  }, []);
-
-  // const getPokemonData = useCallback(async (url) => {
-  //   console.log("getPokemons");
-  //   const res = await fetch(url);
-  //   const data = await res.json();
-  //   const { id, name, height, weight, types, species, abilities } = data;
-  //   // console.log("data", data);
-  //   const getTypes = await types.map((data) => data.type.name);
-  //   const getAbilities = await abilities.map((data) => data.ability.name);
-  //   // console.log("id", id, name, getTypes);
-  //   return {
-  //     id,
-  //     name: name[0].toUpperCase() + name.slice(1),
-  //     getTypes,
-  //     getAbilities,
-  //     height,
-  //     weight,
-  //     species: species.name,
-  //     imgUrl: data.sprites.other["official-artwork"]["front_default"],
-  //   };
-  // }, []);
-
   const prevClickHandler = useCallback(() => {
     console.log("prev clicked");
-    setGetNewNames(true);
+    setIsFetching(true);
     setUrl(prev);
   }, [prev]);
 
   const nextClickHandler = useCallback(() => {
     console.log("next clicked");
-    setGetNewNames(true);
+    setIsFetching(true);
     setUrl(next);
   }, [next]);
 
@@ -59,70 +31,49 @@ const Main = () => {
     const getPokemons = async (url) => {
       console.log("getName start");
       try {
-        fetchJSONData(url)
+        fetch(url)
+          .then((res) => res.json())
           .then((data) => {
             setNames(data.results);
             setPrev(data.previous);
             setNext(data.next);
-          })
-          .then(() => {
-            setGetNewNames(false);
+            setIsFetching(false);
           });
       } catch (err) {
-        setGetNewNames(false);
+        setIsFetching(false);
         console.log(err.message);
       }
     };
 
-    // const fetchPokemonsInfo = async () => {
-    //   // console.log("getpokemon data start");
-    //   try {
-    //     setStatus("LOADING");
-    //     const pokemonsInfo = await Promise.all(
-    //       names.map(async (data) => {
-    //         const pokemonData = await getPokemonData(data.url);
-    //         // console.log("pokemonData", pokemonData);
-    //         return {
-    //           id: pokemonData.id,
-    //           name: pokemonData.name,
-    //           types: pokemonData.getTypes,
-    //           imgUrl: pokemonData.imgUrl,
-    //           abilities: pokemonData.getAbilities,
-    //           height: pokemonData.height,
-    //           weight: pokemonData.weight,
-    //           species: pokemonData.species,
-    //         };
-    //       })
-    //     );
-
-    //     setPokemonsData(pokemonsInfo);
-    //     // setStatus("COMPLETE");
-
-    //     // console.log("pokemonsData", pokemonsInfo);
-    //   } catch (err) {
-    //     setStatus("ERROR");
-    //     console.log(err.message);
-    //   }
-    // };
-    if (getNewNames) getPokemons(url);
-  }, [fetchJSONData, names, getNewNames, url]);
+    if (isFetching) getPokemons(url);
+  }, [isFetching, url]);
 
   return (
     <div className={classes.main}>
-      <Button className={classes["button-left"]} onClick={prevClickHandler}>
-        {/* {prev && ( */}
-        <i className={`fas fa-chevron-left arrow ${classes["arrow-left"]}`}></i>
-        {/* )} */}
+      <Button
+        className={classes["button-left"]}
+        name="prev"
+        onClick={prevClickHandler}
+      >
+        {prev && (
+          <i
+            className={`fas fa-chevron-left arrow ${classes["arrow-left"]}`}
+          ></i>
+        )}
       </Button>
 
       <Pokedex names={names} />
 
-      <Button className={classes["button-right"]} onClick={nextClickHandler}>
-        {/* {next && ( */}
-        <i
-          className={`fas fa-chevron-right arrow ${classes["arrow-right"]}`}
-        ></i>
-        {/* )} */}
+      <Button
+        className={classes["button-right"]}
+        name="next"
+        onClick={nextClickHandler}
+      >
+        {next && (
+          <i
+            className={`fas fa-chevron-right arrow ${classes["arrow-right"]}`}
+          ></i>
+        )}
       </Button>
     </div>
   );
